5d0db8a26062e752dc5e4f955fdba8b1f0e60ecc
MMMziemniaki <trupwszafie@o2.pl>
2018-11-04 13:59:33 +0100

    Getting rid of splash.

--- fafik/spiders/total_points_spider.py
+++ fafik/spiders/total_points_spider.py
@@ -1,53 +1,24 @@
 import scrapy
-import scrapy_splash
 import csv
+import json
 
 
 class TotalPointsSpider(scrapy.Spider):
     name = "total_points"
 
-    lua_script = """
-    function tablesToString(tables)
-        local ret = ""
-        for _, value in ipairs(tables)
-        do
-            ret = ret .. value .. '\\n'
-        end
-
-        return ret
-    end
-
-    function main(splash)
-        assert(splash:go(splash.args.url))
-        local tables = {}
-        repeat
-            local pageNumber = splash:select('p.ism-pagination__info strong')
-            assert(pageNumber)
-            print("Page number " .. pageNumber:text())
-
-            tables[tonumber(pageNumber:text())] = splash:select('table'):info().html
-            assert(tables[tonumber(pageNumber:text())])
-
-            local button = splash:select('a.paginationBtn:nth-child(4):not(.paginationBtn.inactive)')
-            if button then
-                assert(button:mouse_click())
-            end
-        until not button
-
-        return tablesToString(tables)
-    end
-    """
-
     def start_requests(self):
         urls = [
-                'https://fantasy.premierleague.com/a/statistics/total_points'
+                'https://fantasy.premierleague.com/drf/bootstrap-static'
         ]
         for url in urls:
-            yield scrapy_splash.SplashRequest(url=url, callback=self.parse, endpoint='execute', args={'lua_source': TotalPointsSpider.lua_script})
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        with open('out.json', 'w') as f:
+            f.write(response.text)
+        data = json.loads(response.text)
+        data['elements'].sort(reverse = True, key = lambda player : player['total_points'])
         with open('out.csv', 'w') as f:
-            wr = csv.writer(f)
-            wr.writerows([[td.extract() for td in row.css('td a.ismjs-show-element::text,td:not(.ism-table--el__status):not(.ism-table--el__primary)::text')] for row in response.css('tr') if row.css('td')])
-
-
+            writer = csv.writer(f)
+            writer.writerow(['Name', 'Cost', 'Selected by', 'Form', 'Points']);
+            writer.writerows([[player['web_name'], player['now_cost'], player['selected_by_percent'], player['form'], player['total_points']] for player in data['elements']])
